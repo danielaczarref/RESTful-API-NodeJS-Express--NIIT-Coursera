80529731d77a039d33b36c7492cbd1872a73cc7c
danielaczarref <daniela.czarref@gmail.com>
2025-01-26 20:19:47 -0300

    adding swagger

--- User/app.js
+++ User/app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const config = require('./config')
 const userRouter = require('./user')
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDoc = YAML.load('./api-docs/swagger.yaml')
 
 const LoggerMiddleware = (req, res, next) => {
     console.log(`Logged ${req.url} ${req.method}`)
     next()
 }
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 app.use(LoggerMiddleware)
 app.use(express.json())
 app.use('/api/v1/users', userRouter)
