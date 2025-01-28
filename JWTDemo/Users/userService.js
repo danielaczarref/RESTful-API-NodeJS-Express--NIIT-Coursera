0bf4639fd9a896fae7a3fec7a24ffca9d5b4e048
danielaczarref <daniela.czarref@gmail.com>
2025-01-27 21:43:04 -0300

    JWT DEMO - web token, controller, middleware, dao, router, services for auth and user

---
+++ JWTDemo/Users/userService.js
@@ -0,0 +1,11 @@
+const userDAO = require('./userDAO')
+
+function findUser(email, done) {
+    userDAO.findUser(email, done)
+}
+
+function registerUser(userData, done) {
+    userDAO.registerUser(userData, done)
+}
+
+module.exports = { findUser, registerUser }
---
+++ JWTDemo/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const config = require('./config')
+const authRouter = require('./Authentication')
+const userRouter = require('./Users')
+const verifyAuth = require('./Authentication/authMiddleware')
+const dateFormat = require('date-format')
+const morgan = require('morgan')
+
+app.use(express.json())
+morgan.token('time', () => dateFormat.asString(dateFormat.ISO8601_FORMAT, new Date()))
+app.use(morgan('[:time] :remote-addr :method :url :status :res[content-length] :response-time ms'))
+app.use('/auth', authRouter)
+app.use('/users', verifyAuth, userRouter)
+app.listen(config.PORT, () => {
+    console.log(`Listening on port ${config.PORT}`)
+})
